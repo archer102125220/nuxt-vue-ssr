d0deb976f0b0161f834422189a3aef96c279c994
parker <parker@redso.com.hk>
2020-09-23 17:57:05 +0800

    add @nuxtjs/axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 import _ from 'lodash';
 import qs from 'qs';
+import client from './../config/client';
 
-const  baseURL = process.env.api;
+// const baseURL = process.env.api;
+const baseURL = client.api;
 //透過axios向API請求資料
 
-const ax = axios.create({ baseURL }); //建立與API跟目錄連線與操作的物件
+// const ax = axios.create({ baseURL }); //建立與API跟目錄連線與操作的物件
 
-function fetch(_method = 'GET', url, _params = {}, _extendOption = {}) {
+// function fetch(_method = 'GET', url, _params = {}, _extendOption = {}) {
+function fetch(_method = 'GET', url, _params = {}, _extendOption = {}, ax) {
   const method = _.toUpper(_method);
   let params = {};
   // method.match(/GET/)  //match比對完的結果會回傳一個OBJ，而屬性input則為method的值，故 字串.match(正規表示).input 可用於switch-case裡面
@@ -34,12 +37,29 @@ function fetch(_method = 'GET', url, _params = {}, _extendOption = {}) {
   }).then((response) => response.data);
 }
 
-export default {
-  ...axios,
-  fetch,
-  get:(url, _params = {}, _extendOption = {}) => fetch('GET', url, _params, _extendOption),
-  post:(url, _params = {}, _extendOption = {}) => fetch('POST', url, _params, _extendOption),
-  put:(url, _params = {}, _extendOption = {}) => fetch('PUT', url, _params, _extendOption),
-  patch:(url, _params = {}, _extendOption = {}) => fetch('PATCH', url, _params, _extendOption),
-  delet:(url, _params = {}, _extendOption = {}) => fetch('DELET', url, _params, _extendOption)
-};
+export default function ({ $axios }) {
+  const ax = $axios.create({ baseURL }); //建立與API跟目錄連線與操作的物件
+  $axios.get = (url, _params = {}, _extendOption = {}) => fetch('GET', url, _params, _extendOption, ax);
+  $axios.post = (url, _params = {}, _extendOption = {}) => fetch('POST', url, _params, _extendOption, ax);
+  $axios.put = (url, _params = {}, _extendOption = {}) => fetch('PUT', url, _params, _extendOption, ax);
+  $axios.patch = (url, _params = {}, _extendOption = {}) => fetch('PATCH', url, _params, _extendOption, ax);
+  $axios.delet = (url, _params = {}, _extendOption = {}) => fetch('DELET', url, _params, _extendOption, ax);
+  $axios.interceptors.response.use(
+    (response) => {
+      return response && response.data;
+    },
+    (error) => {
+      return Promise.reject(error.response && error.response.data);
+    }
+  );
+}
+
+// export default {
+//   ...axios,
+//   fetch,
+//   get:(url, _params = {}, _extendOption = {}) => fetch('GET', url, _params, _extendOption),
+//   post:(url, _params = {}, _extendOption = {}) => fetch('POST', url, _params, _extendOption),
+//   put:(url, _params = {}, _extendOption = {}) => fetch('PUT', url, _params, _extendOption),
+//   patch:(url, _params = {}, _extendOption = {}) => fetch('PATCH', url, _params, _extendOption),
+//   delet:(url, _params = {}, _extendOption = {}) => fetch('DELET', url, _params, _extendOption)
+// };
